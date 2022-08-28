eafb1fd2684cc601642ec50148f267db430345ce
Margarita Vazhenina <vazhenina.margo@yandex.ru>
2022-08-28 03:13:52 +0300

    Refactoring of UserLoginTests. Replace @Before annotation with @BeforeClass

--- src/test/java/user/UserLoginTests.java
+++ src/test/java/user/UserLoginTests.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import praktikum.LoginUserResponseModel;
 import praktikum.UserCreateModel;
@@ -13,14 +14,14 @@ import praktikum.UserLoginModel;
 
 public class UserLoginTests {
 
-    private final String email = "rat@ratmail.rat";
-    private final String password = "1234";
-    private final String name = "Oleg";
+    private final static String email = "rat@ratmail.rat";
+    private final static String password = "1234";
+    private final static String name = "Oleg";
 
-    private UserLoginModel userLoginModel;
+    private static UserLoginModel userLoginModel;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
 
         UserCreateModel userCreateModel = new UserCreateModel(
